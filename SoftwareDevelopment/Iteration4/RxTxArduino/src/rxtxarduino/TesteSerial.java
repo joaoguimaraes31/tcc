143e50111e00b44f3ae5d589d114fa321e762656
Joao Guimaraes <joaoguimaraes31@gmail.com>
2017-06-08 09:43:35 -0300

    initiating iteration 4

---
+++ SoftwareDevelopment/Iteration4/RxTxArduino/src/rxtxarduino/TesteSerial.java
@@ -0,0 +1,12 @@
+
+public class TesteSerial {
+
+	//private static final int SAMPLES_TO_READ = 2;
+	
+	/*public static void main(String[] args) {
+		Communicator communicator = new Communicator();
+		communicator.connect();
+		communicator.initListener(SAMPLES_TO_READ);
+	}*/
+
+}
