18ece42de6b4d35ab2585f8028c9d955fabe11a5
Joao Guimaraes <joaoguimaraes31@gmail.com>
2017-08-29 18:02:56 -0300

    test2

---
+++ SoftwareDevelopment/BrakeTestBench/src/controllers/SensorSetupController.java
@@ -0,0 +1,57 @@
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import views.*;
+
+public class SensorSetupController {
+
+    //upper controller
+    private NavigationController navigationController;
+
+    //MVC
+    private SensorSetupView view;
+    private ActionListener actionListenerReturnBT, actionListenerserialBT, actionListenerExitBT;
+
+    public SensorSetupController() {
+        //createView();        
+    }
+
+    public void setNavigationController(NavigationController navigationController) {
+        this.navigationController = navigationController;
+    }
+
+    //CRIAR TELA INICIAL E ADCIONAR EVENTOS
+    public void createView() {
+        view = new SensorSetupView();
+        view.setVisible(true);
+        addListeners();
+    }
+
+    //addListeners for controlling view and triggers events
+    public void addListeners() {
+        actionListenerReturnBT = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                view.dispose();
+                navigationController.getInitialScreenController().createView(navigationController.getSerialPortSetupController().getModel().getSelectedSerialPort());
+            }
+        };
+        view.getReturnBT().addActionListener(actionListenerReturnBT);
+
+        actionListenerserialBT = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                view.dispose();
+                navigationController.getSerialPortSetupController().createView();
+            }
+        };
+        view.getSerialBT().addActionListener(actionListenerserialBT);
+
+        actionListenerExitBT = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.exit(0);
+            }
+        };
+        view.getExitBT().addActionListener(actionListenerExitBT);
+    }
+
+}
